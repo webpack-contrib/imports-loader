975fc0cb70f5b5bc5cf3343f7da036c034407eb5
Alexey Lavinsky <cap-Bernardito@yandex.ru>
2021-02-01 14:36:17 +0300

    refactor: drop webpack@4 (#95)
    BRAKING CHANGE: minimum supported `webpack` version is `5`

--- test/fixtures/inline2.js
+++ test/fixtures/inline2.js
@@ -1,1 +1,1 @@
-require('../../src/cjs.js?type=commonjs&imports[]=single|lib_2|lib_2_all&imports[]=multiple|lib_2|lib_2_method|lib_2_method_alias!./some-library.js');
+require('../../src/cjs.js?type=commonjs&imports=single|lib_2|lib_2_all,multiple|lib_2|lib_2_method|lib_2_method_alias!./some-library.js');
